bb829b506afe5dd8ffe3601921ff151cf85c2406
Jean Menezes <jeanmenezes.nave@gmail.com>
2017-08-23 14:43:14 -0300

    Add params for hqs (#2)
    * Add map + pins
    
    * Add pseudo links
    
    * Add hover panorama
    
    * Fix pins responsive behavior
    
    * Add page switch behavior
    
    * Add styles for p2

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@
           },
           visible: true
     });
-
     // 360 degrees of streetview panorama
     let degree = panorama.getPov().heading;
     setTimeout(() => {
@@ -37,5 +36,21 @@
 
   Array.from(document.querySelectorAll(".pin"))
        .forEach(pin => pin.addEventListener("mouseover", panoramaForPin, false))
+  
+  Array.from(document.querySelectorAll(".pin")).forEach(pin => {
+    pin.addEventListener(
+      "click",
+      () => {
+        document.getElementById("p1").className += " hidden";
+        document.getElementById("p2").className += " visible";
+        console.log(pin.getAttribute("data-hq"))
+      })
+  });
+
+  document.getElementById("back").addEventListener("click", () => {
+    document.getElementById("p1").className = "pin-wrapper";
+    document.getElementById("p2").className = "data-wrapper";
+  });
+
 
 })();
